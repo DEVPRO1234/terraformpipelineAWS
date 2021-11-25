0912b972e0589b87e79b4d033e5a42406aa9b761
DEVPRO1234 <95017390+DEVPRO1234@users.noreply.github.com>
2021-11-25 20:12:03 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws"
  region ="us-east-1"
- access_key = "AKIA5W5ZVYOX47CK747T"
- secret_key = "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"
+ access_key = "AKIARUMJ2LF6TAYXYB5N"
+ secret_key = "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"
  }
  
  
 
- resource "aws_instance" "My First Server" {
+ resource "aws_instance" "My-First-Server" {
  ami = "ami-020db2c14939a8efb"
  instance_type = "t2.micro"
  }
