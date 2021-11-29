ee52994ba48730ef82c009333ae82eea148e6f47
DEVPRO1234 <95017390+DEVPRO1234@users.noreply.github.com>
2021-11-29 17:29:12 +0500

    Update

--- variables.tf
+++ variables.tf
@@ -1,7 +1,4 @@
-variable "aws_region" {
-  description = "The AWS region to create things in."
-  default     = "us-east-2"
-}
+
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
