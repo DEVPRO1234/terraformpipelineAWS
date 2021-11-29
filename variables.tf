6a54ac859c637529a31b4ada1c0ab51bc5f510c7
DEVPRO1234 <95017390+DEVPRO1234@users.noreply.github.com>
2021-11-29 20:02:28 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,10 @@
 
 
-variable "key_name" {
-  description = " SSH keys to connect to ec2 instance"
-  default     =  "myJune2021Key"
-}
-
 variable "instance_type" {
   description = "instance type for ec2"
   default     =  "t2.micro"
 }
+
 
 variable "security_group" {
   description = "Name of security group"
